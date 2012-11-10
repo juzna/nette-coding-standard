3a782594f13bf45cb09f85905fb75b1ffca90b4b
Jan Dolecek <juzna.cz@gmail.com>
2012-11-10 15:51:57 +0100

    MustInheritNetteObjectSniff implemented

--- NetteStandard/Sniffs/Classes/MustInheritNetteObjectSniff.php
+++ NetteStandard/Sniffs/Classes/MustInheritNetteObjectSniff.php
@@ -16,14 +16,61 @@ class MustInheritNetteObjectSniff implements PHP_CodeSniffer_Sniff
 	public function register()
 	{
 		return array(
-			T_ABSTRACT,
-			T_EXTENDS
+			T_CLASS,
 		);
 	}
 
 	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
 	{
-		// TODO: Implement process() method.
+		$tokens = $phpcsFile->getTokens();
+
+		$pExtends = $phpcsFile->findNext(T_EXTENDS, $stackPtr, $tokens[$stackPtr]['scope_opener']);
+		if ( $pExtends === FALSE) {
+			$phpcsFile->addError('Class not extending anything', $stackPtr);
+			return;
+		}
+
+		$pEnd = $phpcsFile->findNext(
+			array(T_NS_SEPARATOR, T_STRING, T_WHITESPACE),
+			($pExtends + 1),
+			null,
+			/* exclude */true
+		);
+
+		$extends = trim($phpcsFile->getTokensAsString(($pExtends + 1), ($pEnd - $pExtends - 1)));
+		$extends = $this->covertToFullClassName($phpcsFile, $stackPtr, $extends);
+
+		if ($extends !== 'Nette\Object' && ! $this->isNetteObjectDescendant($extends)) {
+			$phpcsFile->addError('Class not extending Nette\Object', $stackPtr);
+		}
+	}
+
+
+
+	/**
+	 * Convert local class name to full name
+	 * @param  \PHP_CodeSniffer_File
+	 * @param  int
+	 * @param  string
+	 * @return string
+	 */
+	private function covertToFullClassName(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $extends)
+	{
+		// TODO: process all namespace + use statements to translate relative class name to a full name
+		return ltrim($extends, '\\');
+	}
+
+
+
+	/**
+	 * Check that a given class is descendant of Nette\Object
+	 * @param  string
+	 * @return bool
+	 */
+	private function isNetteObjectDescendant($extends)
+	{
+		// FIXME: this may be difficult when using only tokenizer
+		return TRUE;
 	}
 
 }
