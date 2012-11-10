e2601fa6c3a54e9dadef92e46ac44ee8137cd28e
Jan Dolecek <juzna.cz@gmail.com>
2012-11-10 15:05:40 +0100

    WeakTypesComparisonsWithExplanationSniff implemented

--- NetteStandard/Sniffs/ControlStructures/WeakTypesComparisonsWithExplanationSniff.php
+++ NetteStandard/Sniffs/ControlStructures/WeakTypesComparisonsWithExplanationSniff.php
@@ -14,6 +14,13 @@ use PHP_CodeSniffer_File;
  */
 class WeakTypesComparisonsWithExplanationSniff implements PHP_CodeSniffer_Sniff
 {
+	/** @var array When an operator is used, then commend including the text must follow */
+	public $commentMustInclude = array(
+		T_IS_EQUAL => 'intentionally ==',
+		T_IS_NOT_EQUAL => 'intentionally !=',
+	);
+
+
 
 	public function register()
 	{
@@ -23,9 +30,38 @@ class WeakTypesComparisonsWithExplanationSniff implements PHP_CodeSniffer_Sniff
 		);
 	}
 
+
+
 	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
 	{
-		// TODO: Implement process() method.
+		$tokens = $phpcsFile->getTokens();
+
+		$stackPtrOperator = $stackPtr;
+		$operatorCode = $tokens[$stackPtr]['code'];
+
+		$hasComment = FALSE;
+
+		// Read tokens until EOL
+		for (; isset($tokens[$stackPtr]); $stackPtr++) {
+			$code = $tokens[$stackPtr]['code'];
+			$content = $tokens[$stackPtr]['content'];
+
+			switch ($code) {
+				case T_WHITESPACE:
+					if (strpos($content, "\n") !== FALSE) break 2; // newline found -> stop reading
+					break;
+
+				case T_COMMENT:
+					if (strpos($content, $this->commentMustInclude[$operatorCode]) !== FALSE) $hasComment = TRUE;
+					if (strpos($content, "\n") !== FALSE) break 2; // newline found -> stop reading
+					break;
+
+			}
+		}
+
+		if ( ! $hasComment) {
+			$phpcsFile->addError("Equals comparison must be commented with its purpose", $stackPtrOperator);
+		}
 	}
 
 }
